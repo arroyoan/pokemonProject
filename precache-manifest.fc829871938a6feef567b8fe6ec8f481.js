86f3896bec44992f14ca74a0ab5f3f3da1010183
arroyoan <anoel1214@gmail.com>
2020-07-21 12:16:41 -0700

    Updates

---
+++ precache-manifest.fc829871938a6feef567b8fe6ec8f481.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605def56bdf52985f6e29d23347bf5a9",
+    "url": "/pokemonProject/index.html"
+  },
+  {
+    "revision": "661505bd2076fe06afda",
+    "url": "/pokemonProject/static/css/main.27fa993c.chunk.css"
+  },
+  {
+    "revision": "eb685d060fa72a759f30",
+    "url": "/pokemonProject/static/js/2.88fe8b60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemonProject/static/js/2.88fe8b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661505bd2076fe06afda",
+    "url": "/pokemonProject/static/js/main.f3234bc4.chunk.js"
+  },
+  {
+    "revision": "86d55e42ab66bb009fa2",
+    "url": "/pokemonProject/static/js/runtime-main.57eb0f75.js"
+  },
+  {
+    "revision": "fb3bdf5b2c60fc0bbc6924e00889ce2c",
+    "url": "/pokemonProject/static/media/background.fb3bdf5b.png"
+  },
+  {
+    "revision": "55eae3f233d9c7018ed6b92516e848e0",
+    "url": "/pokemonProject/static/media/logo.55eae3f2.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/pokemonProject/static/media/spinner.235b9b3e.gif"
+  }
+]);
